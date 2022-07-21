cc2acf3d578dcdd16a06527877641ce1531fdfc5
hollywoodsie <kobalioff28@gmail.com>
2022-07-21 20:11:41 +0300

    Project primary configuration

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
+require('dotenv').config({ path: './.env' });
 const express = require('express');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 const app = express();
 
-app.listen(PORT, () => console.log(`Server started on ${port}`));
+app.get('/', (req, res) => {
+  res.send('some response');
+});
+app.listen(port, () => console.log(`Server started on ${port}`));
