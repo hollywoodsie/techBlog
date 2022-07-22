f76d5b9b769d70ff34531aef510598d89734087b
hollywoodsie <kobalioff28@gmail.com>
2022-07-22 21:26:18 +0300

    authentification validator added

---
+++ api/validators/auth.validation.js
@@ -0,0 +1,8 @@
+import { body } from 'express-validator';
+
+export const registerValidation = [
+  body('email', 'Invalid email').isEmail(),
+  body('password', 'Invalid password').isLength({ min: 5 }),
+  body('fullName', 'Invalid name').isLength({ min: 3 }),
+  body('avatarUrl', 'Invalid avatar url').optional().isURL(),
+];
