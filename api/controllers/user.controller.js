255ca13f4b82a5613b926c42b68908eb1eb86d40
hollywoodsie <kobalioff28@gmail.com>
2022-07-22 22:49:30 +0300

    user document creation logic

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,24 @@
+import UserModel from '../models/user.model.js';
+
+const register = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
+
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash,
+  });
+
+  const user = await doc.save();
+
+  res.json(user);
+};
