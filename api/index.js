76f4edcd89ae31714b4d44ffa38c9f34a77d258e
hollywoodsie <kobalioff28@gmail.com>
2022-07-22 20:12:37 +0300

    changes for modular approach

--- api/index.js
+++ api/index.js
@@ -1,11 +1,9 @@
 require('dotenv').config({ path: './.env' });
 const express = require('express');
-
+const userRouter = require('./routes/user.routes');
 const port = process.env.PORT || 8000;
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('some response');
-});
+app.use(express.json());
+app.use('/api', userRouter);
 app.listen(port, () => console.log(`Server started on ${port}`));
