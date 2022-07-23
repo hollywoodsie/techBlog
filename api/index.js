14e8c52feada70be62dac3b4f7eaffd5bd3c275d
hollywoodsie <kobalioff28@gmail.com>
2022-07-23 17:02:02 +0300

    router added

--- api/index.js
+++ api/index.js
@@ -1,11 +1,7 @@
 import 'dotenv/config';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
 import express from 'express';
 import mongoose from 'mongoose';
-import { registerValidation } from './validators/auth.validation.js';
-import { validationResult } from 'express-validator';
-import UserModel from './models/user.model.js';
+import router from './routes/user.routes.js';
 mongoose
   .connect(process.env.DB_HOST)
   .then(() => console.log('Database connected...'))
@@ -16,6 +12,6 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.json());
+app.use('/', router);
 
-app.post('auth/register', registerValidation, async (req, res) => {});
 app.listen(port, () => console.log(`Server started on ${port}`));
