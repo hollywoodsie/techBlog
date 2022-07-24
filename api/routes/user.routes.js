37c90d4e0d0b2fa973741241214da6473d139edc
hollywoodsie <kobalioff28@gmail.com>
2022-07-24 17:24:43 +0300

    routes for posts added(refact. later)

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,15 +1,25 @@
 import express from 'express';
-import { registerValidation } from '../validators/auth.validation.js';
+import {
+  loginValidation,
+  registerValidation,
+} from '../validators/auth.validation.js';
+import { postCreateValidation } from '../validators/post.validation.js';
 import { register, login, getMe } from '../controllers/user.controller.js';
 import { checkAuth } from '../middleware/checkAuth.js';
+import { create, getAll, getOne } from '../controllers/post.controller.js';
+
 const router = express.Router();
 
+//user routes
 router.post('/auth/register', registerValidation, register);
-router.post('/auth/login', registerValidation, login);
-router.post('/auth/me', checkAuth, getMe);
+router.post('/auth/login', loginValidation, login);
+router.get('/auth/me', checkAuth, getMe);
 
-router.get('/', (req, res) => {
-  res.send('HOMEPAGE');
-});
+//post routes
+router.post('/posts', checkAuth, postCreateValidation, create);
+router.get('/posts', getAll);
+router.get('/posts/:id', getOne);
+// router.put('/posts', update);
+// router.delete('/posts', remove);
 
 export default router;
