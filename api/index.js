ef11358892e163a8cfaafbdcb4704001825cb798
hollywoodsie <kovalioff28@gmail.com>
2022-07-27 02:28:45 +0300

    added cors

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
 import router from './routes/user.routes.js';
+import cors from 'cors';
 mongoose
   .connect(process.env.DB_HOST)
   .then(() => console.log('Database connected...'))
@@ -10,8 +11,9 @@ mongoose
 const port = process.env.PORT || 8000;
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
+
 app.use('/', router);
 
 app.listen(port, () => console.log(`Server started on ${port}`));
