07cb7bf8c1a2062b178a35b596e60d117cb9d712
hollywoodsie <kovalioff28@gmail.com>
2022-07-28 23:43:34 +0300

    deleted login validation(useless)

--- api/validators/auth.validation.js
+++ api/validators/auth.validation.js
@@ -1,13 +1,12 @@
 import { body } from 'express-validator';
 
-export const loginValidation = [
-  body('email', 'Invalid email').isEmail(),
-  body('password', 'Invalid password').isLength({ min: 5 }),
-];
-
 export const registerValidation = [
   body('email', 'Invalid email').isEmail(),
-  body('password', 'Invalid password').isLength({ min: 5 }),
-  body('fullName', 'Invalid name').isLength({ min: 3 }),
+  body('password', 'Password must be at least 5 characters long').isLength({
+    min: 5,
+  }),
+  body('fullName', 'Full name must be at least 3 characters long').isLength({
+    min: 3,
+  }),
   body('avatarUrl', 'Invalid avatar url').optional().isURL(),
 ];
