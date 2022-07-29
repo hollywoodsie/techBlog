d6dd8215c6f0092ca00139b58241fd5b11b6ac35
hollywoodsie <kovalioff28@gmail.com>
2022-07-29 22:21:47 +0300

    fixed incorrect tags validation

--- api/validators/post.validation.js
+++ api/validators/post.validation.js
@@ -3,6 +3,6 @@ import { body } from 'express-validator';
 export const postCreateValidation = [
   body('title', 'No article title').isLength({ min: 3 }).isString(),
   body('text', 'No article content').isLength({ min: 3 }).isString(),
-  body('tags', 'Invalid tags').optional().isString(),
+  body('tags', 'Invalid tags').optional().isArray(),
   body('imageUrl', 'Invalid ImageURL').optional().isString(),
 ];
