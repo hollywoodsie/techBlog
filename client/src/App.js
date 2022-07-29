a0b734febb869775e5c2a37c7b833a60d06d9a23
hollywoodsie <kovalioff28@gmail.com>
2022-07-29 22:46:06 +0300

    main routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import Container from '@mui/material/Container';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { Routes, Route } from 'react-router-dom';
 import { Header } from './components';
 import { Home, FullPost, Registration, AddPost, Login } from './pages';
+import { fetchAuthMe, selectIsAuth } from './redux/slices/auth';
 
 function App() {
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
+  React.useEffect(() => {
+    dispatch(fetchAuthMe());
+  }, [dispatch]);
+
   return (
     <>
       <Header />
