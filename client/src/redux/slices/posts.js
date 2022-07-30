9ef31da49d2628e104a08ba3e22c9ffcb30cb42c
hollywoodsie <kovalioff28@gmail.com>
2022-07-30 21:46:53 +0300

    removePost req fetching added

--- client/src/redux/slices/posts.js
+++ client/src/redux/slices/posts.js
@@ -11,6 +11,13 @@ export const fetchTags = createAsyncThunk('posts/fetchTags', async () => {
   return data;
 });
 
+export const fetchRemovePost = createAsyncThunk(
+  'posts/fetchRemovePost',
+  async (id) => {
+    await axios.delete(`/posts/${id}`);
+  }
+);
+
 const initialState = {
   posts: {
     items: [],
@@ -27,6 +34,7 @@ const postSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: {
+    //fetch posts
     [fetchPosts.pending]: (state) => {
       state.posts.items = [];
       state.posts.status = 'loading';
@@ -39,6 +47,7 @@ const postSlice = createSlice({
       state.posts.items = [];
       state.posts.status = 'error';
     },
+    //fetch tags
     [fetchTags.pending]: (state) => {
       state.tags.items = [];
       state.tags.status = 'loading';
@@ -50,6 +59,12 @@ const postSlice = createSlice({
     [fetchTags.rejected]: (state) => {
       state.tags.items = [];
       state.tags.status = 'error';
+    },
+    //remove post
+    [fetchRemovePost.pending]: (state, action) => {
+      state.posts.items = state.posts.items.filter(
+        (obj) => obj._id !== action.meta.arg
+      );
     },
   },
 });
