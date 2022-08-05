580da0465f4eef623cdc3f40c7aee229edb73310
hollywoodsie <kovalioff28@gmail.com>
2022-08-05 10:55:07 +0300

    pagination(need fix)+spectag(fixed)

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 import Grid from '@mui/material/Grid';
 import { Pagination } from '@mui/material';
 import { usePagination } from '../components/Pagination';
@@ -8,7 +11,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchPosts, fetchTags } from '../redux/slices/posts';
 import { Post } from '../components/Post';
 import { TagsBlock } from '../components/TagsBlock';
-import { CommentsBlock } from '../components/CommentsBlock';
+import { useParams } from 'react-router-dom';
+import { fetchAuthMe } from '../redux/slices/auth';
 
 export const Home = () => {
   const dispatch = useDispatch();
@@ -16,11 +20,20 @@ export const Home = () => {
   const userData = useSelector((state) => state.auth.data);
   const isPostsLoading = posts.status === 'loading';
   const isTagsLoading = tags.status === 'loading';
+  const [tag, setTag] = React.useState('');
+  const [tabValue, setTabValue] = React.useState('new');
+
+  const handleChangeTab = (event, newValue) => {
+    setTabValue(newValue);
+  };
+  React.useEffect(() => {
+    dispatch(fetchTags());
+    dispatch(fetchAuthMe());
+  }, [dispatch]);
 
   React.useEffect(() => {
-    dispatch(fetchPosts());
-    dispatch(fetchTags());
-  }, [dispatch]);
+    dispatch(fetchPosts(tag));
+  }, [tag]);
 
   const [page, setPage] = React.useState(1);
   const postsPerPage = 5;
@@ -35,63 +48,77 @@ export const Home = () => {
 
   return (
     <>
-      <Tabs
-        style={{ marginBottom: 15 }}
-        value={0}
-        aria-label="basic tabs example"
-      >
-        <Tab label="New" />
-        <Tab label="Popular" />
-      </Tabs>
-      <Grid container spacing={4}>
-        <Grid xs={8} item>
-          {(isPostsLoading
-            ? [...Array(5)]
-            : allDisplayedPosts.currentData()
-          ).map((obj, index) =>
-            isPostsLoading ? (
-              <Post key={index} isLoading={true} />
-            ) : (
-              <Post
-                id={obj._id}
-                title={obj.title}
-                imageUrl={obj.imageUrl ? obj.imageUrl : ''}
-                user={obj.user}
-                createdAt={obj.createdAt}
-                viewsCount={obj.viewsCount}
-                commentsCount={3}
-                tags={obj.tags}
-                isEditable={userData?._id === obj.user._id}
-              />
-            )
-          )}
-        </Grid>
+      <TabContext value={tabValue}>
+        <TabList onChange={handleChangeTab} aria-label="lab API tabs example">
+          <Tab label="New" value="new" />
+          <Tab label="Popular" value="popular" />
+        </TabList>
 
-        <Grid xs={4} item>
-          <TagsBlock items={tags.items} isLoading={isTagsLoading} />
-          {/* <CommentsBlock
-            items={[
-              {
-                user: {
-                  fullName: 'Test user1',
-                  avatarUrl:
-                    'https://media-exp1.licdn.com/dms/image/D4E35AQF-oTDoA35z8A/profile-framedphoto-shrink_200_200/0/1654843610141?e=1659463200&v=beta&t=hYYYAabV0OH1oT-fYqqMI47zJiGsm9eZDXZBqXYQ_Mg',
-                },
-                text: 'Test comment',
-              },
-              {
-                user: {
-                  fullName: 'Test user2',
-                  avatarUrl:
-                    'https://media-exp1.licdn.com/dms/image/D4E35AQF-oTDoA35z8A/profile-framedphoto-shrink_200_200/0/1654843610141?e=1659463200&v=beta&t=hYYYAabV0OH1oT-fYqqMI47zJiGsm9eZDXZBqXYQ_Mg',
-                },
-                text: 'When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top',
-              },
-            ]}
-            isLoading={false}
-          /> */}
-        </Grid>
-      </Grid>
+        <TabPanel value="new">
+          <Grid container spacing={4}>
+            <Grid xs={8} item>
+              {(isPostsLoading
+                ? [...Array(5)]
+                : allDisplayedPosts.currentData()
+              ).map((obj, index) =>
+                isPostsLoading ? (
+                  <Post key={index} isLoading={true} />
+                ) : (
+                  <Post
+                    id={obj._id}
+                    title={obj.title}
+                    imageUrl={obj.imageUrl ? obj.imageUrl : ''}
+                    user={obj.user}
+                    createdAt={obj.createdAt}
+                    viewsCount={obj.viewsCount}
+                    commentsCount={3}
+                    tags={obj.tags}
+                    isEditable={userData?._id === obj.user._id}
+                  />
+                )
+              )}
+            </Grid>
+
+            <Grid xs={4} item>
+              <TagsBlock
+                items={tags.items}
+                isLoading={isTagsLoading}
+                setTag={setTag}
+              />
+            </Grid>
+          </Grid>
+        </TabPanel>
+        <TabPanel value="popular">
+          {/* <Grid container spacing={4}>
+            <Grid xs={8} item>
+              {(isPostsLoading
+                ? [...Array(5)]
+                : allDisplayedPosts.currentData()
+              ).map((obj, index) =>
+                isPostsLoading ? (
+                  <Post key={index} isLoading={true} />
+                ) : (
+                  <Post
+                    id={obj._id}
+                    title={obj.title}
+                    imageUrl={obj.imageUrl ? obj.imageUrl : ''}
+                    user={obj.user}
+                    createdAt={obj.createdAt}
+                    viewsCount={obj.viewsCount}
+                    commentsCount={3}
+                    tags={obj.tags}
+                    isEditable={userData?._id === obj.user._id}
+                  />
+                )
+              )}
+            </Grid>
+
+            <Grid xs={4} item>
+              <TagsBlock items={tags.items} isLoading={isTagsLoading} />
+            </Grid>
+          </Grid> */}
+        </TabPanel>
+      </TabContext>
       <Pagination
         color="primary"
         sx={{
